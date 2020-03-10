2dbbd2abf62680ef2c4a73f742eb800bca9bc811
galbeiroc <alcresgu_1989@hotmail.com>
2020-03-10 11:37:10 -0500

    implement component Join and applying style

--- client/src/components/Join/index.js
+++ client/src/components/Join/index.js
@@ -1,11 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+
+import './style.css';
 
 const Join = () => {
-    return (
-        <div>
-            <h1>Join</h1>
-        </div>
-    )
-}
+  const [name, setName] = useState('');
+  const [room, setRoom] = useState('');
 
-export default Join
+  return (
+    <div className="joinOuterContainer">
+      <div className="joinInnerContainer">
+        <h1 className="heading">Join</h1>
+        <div>
+          <input
+            type="text"
+            className="joinInput"
+            placeholder="Name"
+            onChange={e => setName(e.target.value)}
+          />
+        </div>
+        <div>
+          <input
+            type="text"
+            className="joinInput mt-20"
+            placeholder="Room"
+            onChange={e => setRoom(e.target.value)}
+          />
+        </div>
+        <Link
+          onClick={e => (!name || !room ? e.preventDefault() : null)}
+          to={`/chat?name=${name}&room=${room}`}
+        >
+          <button className="button mt-20" type="submit">
+            Sign In
+          </button>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Join;
