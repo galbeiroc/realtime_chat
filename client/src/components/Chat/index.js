a84645ffb9da1897e89f5a72779012ff81c58fd8
galbeiroc <alcresgu_1989@hotmail.com>
2020-03-10 12:05:17 -0500

    implement Chat component, take variables for url with queryString and use socketio

--- client/src/components/Chat/index.js
+++ client/src/components/Chat/index.js
@@ -1,11 +1,27 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import queryString from 'query-string';
+import io from 'socket.io-client';
+import { ENDPOINT } from '../../config/config';
 
-const Chat = () => {
-    return (
-        <div>
-            <h1>Chat</h1>
-        </div>
-    )
-}
+let socket;
 
-export default Chat
+const Chat = ({ location }) => {
+  const [name, setName] = useState('');
+  const [room, setRoom] = useState('');
+  useEffect(() => {
+    const { name, room } = queryString.parse(location.search);
+
+    socket = io(ENDPOINT);
+    console.log(socket);
+    
+    setName(name);
+    setRoom(room);
+  }, [ENDPOINT, location.search]);
+  return (
+    <div>
+      <h1>Chat</h1>
+    </div>
+  );
+};
+
+export default Chat;
